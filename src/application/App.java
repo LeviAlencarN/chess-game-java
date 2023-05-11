9ae789900b673ad6eaca5b609b1e4110537cce92
Levi Alencar <levialencar66@gmail.com>
2023-05-11 15:29:28 -0300

    primeira classe: Position

---
+++ src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardGame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position positon = new Position(3, 4);
+        System.out.println(positon);
+    }
+}
