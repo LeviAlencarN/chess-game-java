f60c6ba45e3efdee55eec0fd5fdd1c1457fb4c9c
Levi Alencar <levialencar66@gmail.com>
2023-05-11 17:24:54 -0300

    comecando a implementar tabuleiro e pecas

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,11 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position positon = new Position(3, 4);
-        System.out.println(positon);
+        Board board = new Board(8, 8);
+        System.out.println(board);
     }
 }
